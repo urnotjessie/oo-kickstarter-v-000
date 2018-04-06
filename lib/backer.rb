1bf8fc95599432ed176a86ae4fa966e59aaa752f
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-06 12:45:16 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ class Backer
     end
 
     def back_project(project)
-        if @backed_projects.include?(project)
+        if !@backed_projects.include?(project)
             @backed_projects << project
             project.add_backer(self)
         end
